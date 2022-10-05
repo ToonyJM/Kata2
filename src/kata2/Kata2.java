1d29db1da489703e207ff52fce6221c50f0cf09e
mm <mm@10.195.87.50>
2022-10-05 09:14:14 +0100

    Commit V1

---
+++ src/kata2/Kata2.java
@@ -0,0 +1,27 @@
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Kata2 {
+    public static void main(String[] args) {
+        int[] data = {9,2,3,4,5,8,1,2,3,9,5,3,6,2,7,3,6,0};
+        Map<Integer,Integer> histogram = new HashMap<Integer,Integer>();
+        
+        for (int i = 0; i < data.length; i++) {
+            if(histogram.containsKey(data[i])) { 
+                histogram.put(data[i], histogram.get(data[i]) + 1);
+            } else {
+                histogram.put(data[i], 1);
+            }
+            
+        }
+        
+        for (Map.Entry<Integer,Integer> entry  : histogram.entrySet()) {
+            System.out.println(entry.getKey() + "====> " + entry.getValue());
+        }
+        
+
+    }
+    
+}
